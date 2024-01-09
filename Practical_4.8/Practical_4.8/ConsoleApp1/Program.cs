e267bae5be4cd61e17ecca513a8752170200e9ba
Alexandr Logvin <alxlogvin@inbox.ru>
2024-01-09 19:36:38 +0500

    pr4.8

---
+++ Practical_4.8/Practical_4.8/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задание 2. Сложение матриц
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов:");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix1 = new int[n, m];
+            int[,] matrix2 = new int[n, m];
+            int[,] matrixSumm = new int[n, m];
+
+            Random r = new Random();
+
+            Console.WriteLine("\nПервая матрица:\n");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix1[i, j] = r.Next(10);
+                    Console.Write($"{matrix1[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nВторая матрица:\n");    
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix2[i, j] = r.Next(10);
+                    Console.Write($"{matrix2[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nСумма двух матриц:\n");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrixSumm[i, j] = matrix1[i, j] + matrix2[i, j];
+                    Console.Write($"{matrixSumm[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practical_4.8/Practical_4.8/Practical_4.8/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задание 1. Случайная матрица
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_4._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов:");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[n, m];
+
+            Random r = new Random();
+
+            int summ = 0;
+            int summ_m = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                summ += summ_m;
+
+                for(int j = 0; j < m; j++) 
+                {
+                    matrix[i, j] = r.Next(10);
+                    summ_m += matrix[i, j];
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                summ = summ_m;
+                Console.WriteLine();
+            }
+            Console.WriteLine($"\nСумма всех элементов матрицы: {summ}");
+            Console.ReadKey();
+        }
+    }
+}
