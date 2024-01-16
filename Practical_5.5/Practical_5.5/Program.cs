1126d9a475b5a5fa5c16e04e2b168cbc7c59085c
Alexandr Logvin <alxlogvin@inbox.ru>
2024-01-16 20:06:09 +0500

    pr5

---
+++ Practical_5.5/Practical_5.5/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задание 1. Метод разделения строки на слова
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string text = (Console.ReadLine());
+
+            string[] arrWords = SplitText(text);
+
+            WriteWords(arrWords);
+        }
+
+        /// <summary>
+        /// Метод разделения предложения на отдельные слова
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string[] SplitText(string text)
+        {
+            string[] arrWords = text.Split(' ');
+            return arrWords; 
+        }
+
+        /// <summary>
+        /// Метод вывода массива в консоль
+        /// </summary>
+        /// <param name="arrWords"></param>
+        static void WriteWords(string[] arrWords)
+        {
+            foreach (var sub in arrWords)
+            {
+                Console.WriteLine(sub);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practical_5.5/Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задание 2. Перестановка слов в предложении
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string inputPhrase = Console.ReadLine();
+
+            string resultPhrase = Reverse(inputPhrase);
+
+            Console.WriteLine(resultPhrase);
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Метод реализующий обратный порядок слов
+        /// </summary>
+        /// <param name="inputPhrase"></param>
+        /// <returns></returns>
+        static string Reverse(string inputPhrase)
+        { 
+           string[] arrWords = SplitText(inputPhrase);
+
+            string reversedPhrase = "";
+
+            for (int i = arrWords.Length - 1; i >= 0; i--)
+            {
+                reversedPhrase += arrWords[i] + " ";
+            }
+            return reversedPhrase;
+
+        }
+
+        /// <summary>
+        /// Метод разделения предложения на отдельные слова
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string[] SplitText(string text)
+        {
+            string[] arrayWords = text.Split(' ');
+            return arrayWords;
+        }
+    }
+}
